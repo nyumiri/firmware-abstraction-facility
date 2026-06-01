677db19b1b745100bf51b4c0fe0515e68804da5b
nyumiri <alice67n@gmail.com>
2026-06-01 20:21:13 -0300

    init: basic projetc structure created

---
+++ README.md
@@ -0,0 +1,17 @@
+# Firmware Abstraction Facility (FAF)
+
+Uma biblioteca modular para controle de hardware, focada em arquitetura profissional e baixo acoplamento
+
+> **Status:** Em refatoração (Branch `Dev` Ativa).
+
+## Arquitetura
+Esta biblioteca utiliza um design modular inspirado em frameworks de baixo nível, permitindo que o usuário inclua apenas o necessário para sua aplicação.
+
+## Roadmap
+- [ ] Estrutura básica de módulos.
+- [ ] API de registro declarativa.
+- [ ] Testes Unitários em Ambiente Desktop a fim de Produtividade.
+- [ ] Exemplos de Uso.
+
+## Contato
+Desenvolvido para garantir controle total sobre os periféricos.
---
+++ include/FAF/FAF.h
@@ -0,0 +1,20 @@
+#ifndef FAF_H
+#define FAF_H
+
+/*
+ * Firmware Abstraction Facility
+ * Copyright (C) 2026 Alice aka Nyumi.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License as published by the Massachusetts Institute of Technology.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MIT License for more details.
+ * 
+ * You should have received a copy of the MIT License along with this program.
+ * If not, see <https://opensource.org/licenses/MIT>.
+ */
+
+#endif // FAF_H
